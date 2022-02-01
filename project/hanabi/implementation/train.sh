869907bf10d0f0043755646a37d52935ed5664dc
Simone Alberto Peirone <simone.peirone.98@gmail.com>
2022-02-01 19:21:04 +0100

    Add training script

---
+++ project/hanabi/implementation/train.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+NUM_TRAINING_PLAYER=5
+N_ITERATIONS=5
+DISCOUNT=0.25
+BATCH_SIZE=64
+INITIAL_EPS=1.0
+EPS_STEP=0.99995
+MINIMUM_EPS=0.1
+TARGET_MODEL_REFRESH_INTERVAL=1000
+EVALUATION_INTERVAL=10000
+EVALUATION_NUM_GAMES=100
+INITIAL_LR=0.001
+LR_STEP=50000
+LR_GAMMA=0.5
+
+for NUM_PLAYERS in 2 3 4 5
+do
+    MODEL_SAVE_PATH="models/DQN_${NUM_PLAYERS}_players"
+
+    python Learning.py --players $NUM_PLAYERS \
+        --training-players $NUM_TRAINING_PLAYER \
+        --iterations $N_ITERATIONS \
+        --discount $DISCOUNT \
+        --batch-size $BATCH_SIZE \
+        --initial-eps $INITIAL_EPS \
+        --eps-step $EPS_STEP \
+        --minimum-step $MINIMUM_EPS \
+        --turn-dependent-eps \
+        --target-refresh-interval $TARGET_MODEL_REFRESH_INTERVAL \
+        --evaluation-interval $EVALUATION_INTERVAL \
+        --evaluation-num-games $EVALUATION_NUM_GAMES \
+        --initial-lr $INITIAL_LR \
+        --lr-step $LR_STEP \
+        --lr-gamma $LR_GAMMA \
+        --model-save-path $MODEL_SAVE_PATH
+done
